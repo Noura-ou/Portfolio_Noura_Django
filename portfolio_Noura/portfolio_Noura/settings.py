db3663df19d1fe7fb1c24d62514c3fc27e1e2823
Noura Ous <noura.ousfia@gmail.com>
2023-05-23 14:09:11 +0200

    update settings

--- portfolio_Noura/portfolio_Noura/settings.py
+++ portfolio_Noura/portfolio_Noura/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-58mn#2vjor$c3#oo$r#7_mcf12=tag*qci_757&ztffm0_a1ct
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app' ,'127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app' ,'127.0.0.1', '.now.sh']
 
 
 # Application definition
@@ -82,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+ }
 
 
 # Password validation
@@ -121,7 +122,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS=[BASE_DIR/"static",]
+#STATICFILES_DIRS=[BASE_DIR/"static",]
+
+
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
