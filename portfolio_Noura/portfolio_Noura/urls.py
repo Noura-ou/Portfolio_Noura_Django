460791c3aa8d7b894cc38c623b790a797eccb395
Noura Ous <noura.ousfia@gmail.com>
2023-05-23 11:47:52 +0200

    commit

--- portfolio_Noura/portfolio_Noura/urls.py
+++ portfolio_Noura/portfolio_Noura/urls.py
@@ -21,5 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api_home.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),   
 ]
