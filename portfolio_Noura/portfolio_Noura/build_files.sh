c6bd3bd3cff13c9eb38be52a5b09666378d78bd1
Noura Ous <noura.ousfia@gmail.com>
2023-05-23 14:56:42 +0200

    modifi build

--- portfolio_Noura/portfolio_Noura/build_files.sh
+++ portfolio_Noura/portfolio_Noura/build_files.sh
@@ -1,4 +1,12 @@
 # build_files.sh
-from ./ import requirements.txt
-pip install -r requirements.txt
+
+requirements_file="../requirements.txt"
+
+if [ -f "$requirements_file" ]; then
+    pip install -r "$requirements_file"
+else
+    echo "Le fichier $requirements_file n'existe pas dans le répertoire parent."
+    exit 1
+fi
+
 python3.9 manage.py collectstatic --no-input
