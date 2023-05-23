95af7ea62eee29a3f0f92bc4b72f347d03df53c6
Noura Ous <noura.ousfia@gmail.com>
2023-05-23 11:30:11 +0200

    finalisation change name

--- portfolio_Noura/manage.py
+++ portfolio_Noura/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'what_to_cook.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_Noura.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
