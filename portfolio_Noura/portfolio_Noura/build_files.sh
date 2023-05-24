7cf097a03431ff89aae99cdae97330c9aa19e75d
Noura Ous <noura.ousfia@gmail.com>
2023-05-24 09:25:01 +0200

    python

--- portfolio_Noura/portfolio_Noura/build_files.sh
+++ portfolio_Noura/portfolio_Noura/build_files.sh
@@ -1,7 +1,7 @@
 # build_files.sh
 
 requirements_file="../requirements.txt"
-
+manage_file="../manage.py"
 
 if [ -f "$requirements_file" ]; then
     pip install -r "$requirements_file"
@@ -11,4 +11,11 @@ else
 fi
 
 
+
+if [ -f "$manage_file" ]; then
+    python $manage_file collectstatic --no-input
+else
+    echo "Le fichier $manage_file n'existe pas dans le répertoire parent."
+    exit 1
+fi
 
