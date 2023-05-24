c1665fbff74776fa0002c22d90b1312827cec919
Noura Ous <noura.ousfia@gmail.com>
2023-05-24 11:29:20 +0200

    modif stat

--- portfolio_Noura/portfolio_Noura/settings.py
+++ portfolio_Noura/portfolio_Noura/settings.py
@@ -131,10 +131,10 @@ STATIC_URL = 'static/'
 
 
 
-#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 
 # Default primary key field type
