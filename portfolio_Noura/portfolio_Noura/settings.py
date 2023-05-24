6ce07ff02df92bdbaa276901d24ba64033a854dc
Noura Ous <noura.ousfia@gmail.com>
2023-05-24 11:21:56 +0200

    modif static

--- portfolio_Noura/portfolio_Noura/settings.py
+++ portfolio_Noura/portfolio_Noura/settings.py
@@ -131,8 +131,11 @@ STATIC_URL = 'static/'
 
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
